5bcd5fa213b1889ecbbd490215415cf0f0453452
Amit2027429 <musafit763@gmail.com>
2026-05-13 12:25:12 +0530

    fixed render startup

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,12 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error(`MongoDB Error: ${error.message}`);
     process.exit(1);
   }
 };
